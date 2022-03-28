92409c5be7ec25038ce4c40e91bdad75aec8d1be
mariabisneta <maria.bisneta88352@alunos.ufersa.edu.br>
2022-03-28 15:29:27 -0300

    adicionando html e css

--- cadpessoas_maria/src/main/java/br/com/maria/cadpessoa/controllers/PessoaController.java
+++ cadpessoas_maria/src/main/java/br/com/maria/cadpessoa/controllers/PessoaController.java
@@ -1,10 +1,20 @@
 package br.com.maria.cadpessoa.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.maria.cadpessoa.repositories.PessoaRepository;
 
 @Controller
 @RequestMapping("/")
 public class PessoaController {
+	@Autowired
+	PessoaRepository pessoaRepo;
 
+	@GetMapping
+	public String index() {
+		return "index.html";
+	}
 }
