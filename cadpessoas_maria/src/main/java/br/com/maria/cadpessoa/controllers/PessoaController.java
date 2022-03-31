85432eb4d4ba98d7c364c46ced6ef9faaf5e660b
mariabisneta <maria.bisneta88352@alunos.ufersa.edu.br>
2022-03-30 23:57:13 -0300

    partes do index funcionando

--- cadpessoas_maria/src/main/java/br/com/maria/cadpessoa/controllers/PessoaController.java
+++ cadpessoas_maria/src/main/java/br/com/maria/cadpessoa/controllers/PessoaController.java
@@ -1,15 +1,20 @@
 package br.com.maria.cadpessoa.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.maria.cadpessoa.model.Pessoa;
 import br.com.maria.cadpessoa.repositories.PessoaRepository;
 
 @Controller
 @RequestMapping("/")
 public class PessoaController {
+	
 	@Autowired
 	PessoaRepository pessoaRepo;
 
@@ -17,4 +22,13 @@ public class PessoaController {
 	public String index() {
 		return "index.html";
 	}
+
+	@GetMapping("/listarPessoas")
+	public ModelAndView listarPessoas() {
+		List<Pessoa> lista = pessoaRepo.findAll();
+		ModelAndView mav = new ModelAndView("listarPessoas");
+		mav.addObject("pessoas", lista);
+		return mav;
+	}
+	
 }
